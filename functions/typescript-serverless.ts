1baccca5872ac7c50092ab9c9134d8dcf84ae505
David Disch <elijahdisch@gmail.com>
2021-10-19 16:55:13 +1000

    Parse context from custom context

--- functions/typescript-serverless.ts
+++ functions/typescript-serverless.ts
@@ -1,12 +1,28 @@
-import { Handler } from '@netlify/functions'
+import { Handler, HandlerContext } from '@netlify/functions'
+
+type NetlifyContext = {
+  site_url: string
+}
+
+function netlifyContextFromHandlerContext(context: HandlerContext): NetlifyContext | undefined {
+  try {
+    return JSON.parse(
+      Buffer
+      .from(context.clientContext.custom.netlify, 'base64')
+      .toString('utf-8')
+    )
+  } catch {
+    return undefined
+  }
+}
 
 const handler: Handler = async (event, context) => {
-  console.log(JSON.stringify(context, null, 2))
-  console.log('context', process.env.CONTEXT)
+  const netlifyContext = netlifyContextFromHandlerContext(context)
+  const functionUrl = netlifyContext?.site_url ?? 'localhost'
 
   return {
     statusCode: 200,
-    body: "🦀 Hello, Netlify from Typescript 🦀",
+    body: `🦀 Hello, Netlify from Typescript (@${functionUrl}) 🦀`,
   }
 }
 

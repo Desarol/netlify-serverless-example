5edde020ede3839dd24ee8ebcb9866957f012dc3
David Disch <elijahdisch@gmail.com>
2021-10-19 16:28:09 +1000

    Log context in function

--- functions/typescript-serverless.ts
+++ functions/typescript-serverless.ts
@@ -1,6 +1,9 @@
 import { Handler } from '@netlify/functions'
 
 const handler: Handler = async (event, context) => {
+  console.log(JSON.stringify(context, null, 2))
+  console.log('context', process.env.CONTEXT)
+
   return {
     statusCode: 200,
     body: "🦀 Hello, Netlify from Typescript 🦀",
